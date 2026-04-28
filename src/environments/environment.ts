4dadaf32f624e982e28ffc2925b60b0a75297bb5
Kevin Quevedo <quevedo.kevin@hotmail.com>
2026-04-28 14:09:04 -0300

    se agregó firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDjf0CxdZo03Tuj4vh54jiaxYJlFht2t0I",
+    authDomain: "pizzeria-quevedo.firebaseapp.com",
+    projectId: "pizzeria-quevedo",
+    storageBucket: "pizzeria-quevedo.firebasestorage.app",
+    messagingSenderId: "20877158167",
+    appId: "1:20877158167:web:0de74827cc0d483f8b7952"
+  }
+};
